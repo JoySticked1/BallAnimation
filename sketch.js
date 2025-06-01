228e4114a7788e03490265a207563385e13d0cc8
JoySticked1 <DavidOliverM10@gmail.com>
2025-06-01 17:35:32 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,87 @@
+let rectX=50;
+let rectY=300;
+let angle=0;
+let ballX;
+let ballY;
+let c1;
+let c2;
+let c3;
+let l=1;
+let checkvar=360;
+let newVar=328;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(0);
+	world.gravity.y=10
+	world.friction=10
+	circls = new Group()
+	circls.d=5
+	circls.collider = "k"
+	circls.mass= 0
+	circls.color='white'
+	circls.bounciness = 1;
+	circls.stroke='white'
+	circls.amount=165
+	circls.overlaps(circls)
+	while (l<165){
+		l+=1
+		new circls.Sprite()
+		circls.x= (i) => 256*cos(i)+windowWidth/2
+		circls.y= (i) => 256*sin(i)+windowHeight/2
+	}
+	balls = new Group()
+	balls.x=windowWidth/2+random(-125,125)
+	balls.y=windowHeight/2+random(-125,-200)
+	balls.d=25
+	balls.bounciness = 1
+	new balls.Sprite()
+	newCircle = new Sprite()
+	newCircle.stroke='black'
+	newCircle.color='black'
+	newCircle.mass=0
+	newCircle.collider = 'none'
+	newCircle.d=5
+	newCircle.x = 256*cos(checkvar)+windowWidth/2
+	newCircle.y = 256*sin(checkvar)+windowHeight/2
+	newCircle.overlaps(circls, overlap)
+	death = new Sprite()
+	death.width=10*windowWidth
+	death.height=50
+	death.color='black'
+	death.mass=0
+	death.x=windowWidth/2
+	death.y=windowHeight-50
+	death.overlaps(circls)
+	death.overlaps(newCircle)
+	death.overlaps(balls, die)
+}
+
+function draw() {
+	death.x=windowWidth/2
+	death.y=windowHeight-50
+	death.velocity.y=0
+	background(0)
+	
+	if (checkvar==359.5){
+		checkvar = -0.5
+	}
+	checkvar+=0.5
+	newCircle.x = 256*cos(checkvar)+windowWidth/2
+	newCircle.y = 256*sin(checkvar)+windowHeight/2
+}
+
+function overlap(newCircle, circl){
+	circl.remove()
+	newVar +=1
+	if (newVar==360){
+		newVar = 0
+	}
+	new circls.Sprite(256*cos(newVar)+windowWidth/2,256*sin(newVar)+windowHeight/2)
+}
+
+function die(death, ball){
+	ball.remove()
+	new balls.Sprite(windowWidth/2+random(-125,125),windowHeight/2+random(-125,-200))
+	new balls.Sprite(windowWidth/2+random(-125,125),windowHeight/2+random(-125,-200))
+}
